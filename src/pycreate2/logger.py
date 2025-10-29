16374a80b9cb552e4a0ed93ecc28e620099eafc9
roomba <roomba>
2025-10-29 17:44:39 +0000

    save logs to file

--- src/pycreate2/logger.py
+++ src/pycreate2/logger.py
@@ -3,5 +3,6 @@ import logging
 logging.basicConfig(
     format='[%(name)-15s %(asctime)s,%(msecs)03d] %(levelname)s in <%(funcName)s>: %(message)s',
     datefmt="%H:%M:%S",
-    level=logging.INFO
+    level=logging.INFO,
+    filename="pycreate2.log",
 )
