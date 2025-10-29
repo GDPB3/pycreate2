8c7d6c07f8628f25d4907683c6cb652d7abb95aa
roomba <roomba>
2025-10-29 18:13:56 +0000

    modify sensorExample

--- examples/sensorExample.py
+++ examples/sensorExample.py
@@ -34,16 +34,17 @@ if __name__ == "__main__":
         PacketNames.LIGHT_BUMP_CENTER_RIGHT,
         PacketNames.LIGHT_BUMP_FRONT_RIGHT,
         PacketNames.LIGHT_BUMP_RIGHT,
+        PacketNames.BATTERY_CHARGE
     ]
     while True:
         # Packet 100 contains all sensor data.
         sensors = bot.get_sensor_list(names)
 
         if cnt % 20 == 0:
-            print("[L ] [LF] [LC] [CR] [RF] [ R]")
+            print("[L ] [LF] [LC] [CR] [RF] [ R]  [BATT]")
 
         print(
-            f"{sensors[names[0]]:4} {sensors[names[1]]:4} {sensors[names[2]]:4} {sensors[names[3]]:4} {sensors[names[4]]:4} {sensors[names[5]]:4}")
+            f"{sensors[names[0]]:4} {sensors[names[1]]:4} {sensors[names[2]]:4} {sensors[names[3]]:4} {sensors[names[4]]:4} {sensors[names[5]]:4}   {sensors[names[6]]:4}")
         time.sleep(.01)
 
         cnt += 1
